f8e69a7a63a17217951b876a48811ada0b14e2be
Justin Stauffer <jstauffer@tenxweb.com>
2019-11-22 12:29:57 -0500

    11-22.1

--- src/components/RequestQuote/requestQuote.js
+++ src/components/RequestQuote/requestQuote.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Example = (props) => {
     return (
@@ -9,33 +8,22 @@ const Example = (props) => {
         }}>
 
 
-            <Form className='text-center' name="requestQuote" method="POST" data-netlify="true" data-netlify-recaptcha="true" style={{
-                maxWidth: '600px',
-                margin: 'auto',
-                border: '3px solid black',
-                padding: '1rem'
-            }}>
+            <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <h1>Request A Quote</h1>
-                <input type="hidden" name="form-name" value="requestQuote" />
-                <FormGroup>
-                    <Label for="exampleEmail">Name</Label>
-                    <Input type="text" name="name" id="Name" placeholder="Enter your name here." />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="Email" placeholder="Enter your email here." />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Message</Label>
-                    <Input type="textarea" name="message" id="Message" placeholder="Enter a message here." />
-                </FormGroup>
-                <div data-netlify-recaptcha="true"></div>
-                <Button className='btn' style={{
-                    background: '#f38c1e',
-                    border: '1px solid #f38c1e',
-                    color: 'black'
-                }}>Submit</Button>
-            </Form>
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
         </div>
     );
 }
